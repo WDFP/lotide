6338922d6c1a59ccd539d5e860a1c9ef8139d51d
Victor Ha <victorha88@gmail.com>
2022-09-29 17:15:25 -0400

    Implement eqArrays

---
+++ eqArray.js
@@ -0,0 +1,39 @@
+// FUNCTION IMPLEMENTATION
+
+const assertEqual = function (actual, expected) {
+  if (actual === expected) {
+    console.log(`👍👌🔔 Assertion Passed: ${actual} === ${expected}`);
+  } else if (actual !== expected) {
+    console.log(`👎🙀🔕 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+// TEST CODE
+
+// assertEqual("Bootcamp", "Bootcamp");
+// assertEqual(5, 4);
+
+const eqArrays = function (arrays1, arrays2) {
+  // if (arrays1 === arrays2) {
+  //   return true;
+  //   }  else {
+  if (arrays1.length !== arrays2.length) {
+    return false;
+  }
+  // need loop to loop through each array to see if the elements are the same and true
+  for (i = 0; i < arrays1.length; i++) {
+    // see if elements are the same
+    // compare all elements in array1 and array2 with the correct indexes.
+    if (arrays1[i] !== arrays2[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+console.log(eqArrays([1, 2, 3], [1, 2, 3])); // => true
+console.log(eqArrays([1, 2, 3], [3, 2, 1])); // => false
+
+console.log(eqArrays(["1", "2", "3"], ["1", "2", "3"])); // => true
+console.log(eqArrays(["1", "2", "3"], ["1", "2", 3])); // => false
+
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS
