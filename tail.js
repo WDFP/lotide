af1108cc659908d98fb9f6dd0a2c8d04594fa276
Victor Ha <victorha88@gmail.com>
2022-10-01 18:17:18 -0400

    Improved understanding of tail.js

--- tail.js
+++ tail.js
@@ -1,6 +1,6 @@
 // FUNCTION IMPLEMENTATION
 
-const assertEqual = function(actual, expected) {
+const assertEqual = function (actual, expected) {
   if (actual === expected) {
     console.log(`👍👌🔔 Assertion Passed: ${actual} === ${expected}`);
   } else if (actual !== expected) {
@@ -8,14 +8,13 @@ const assertEqual = function(actual, expected) {
   }
 };
 
-const tail = function(words) {
-  let NewArray = words.slice(1);
-  return NewArray;
+const tail = function (array) {
+  newArray = [];
+  newArray = array.slice(1);
+  return newArray;
 };
-
-
 
 // Test Case: Check the original array
 const words = ["Yo Yo", "Lighthouse", "Labs"];
-tail(words); // no need to capture the return value since we are not checking it
-assertEqual(words.length, 3); // original array should still have 3 elements!
+console.log(tail(words)); // no need to capture the return value since we are not checking it
+assertEqual(tail(words).length, 2); // original array should still have 3 elements!
