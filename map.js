9eb5555f7a0abc9662a7a35e35695e5ebb3eb395
Victor Ha <victorha88@gmail.com>
2022-10-18 14:01:22 -0400

    M1W2 Implement Map-Lotide

---
+++ map.js
@@ -0,0 +1,39 @@
+const words = ["ground", "control", "to", "major", "tom"];
+
+const map = function (array, callback) {
+  const results = [];
+  for (let item of array) {
+    results.push(callback(item));
+  }
+  return results;
+};
+
+const results1 = map(words, (word) => word[0]);
+console.log(results1);
+
+const eqArrays = function (array1, array2) {
+  if (array1.length !== array2.length) {
+    return false;
+  }
+  // need loop to loop through each array to see if the elements are the same and true
+  for (i = 0; i < array1.length; i++) {
+    // see if elements are the same
+    // compare all elements in array1 and array2 with the correct indexes.
+    if (array1[i] !== array2[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+const assertArraysEqual = function (array1, array2) {
+  if (eqArrays(array1, array2)) {
+    console.log(`👍👌🔔 Assertion Passed: [${array1}] === [${array2}]`);
+  } else {
+    console.log(`👎🙀🔕 Assertion Failed: [${array1}] !== [${array2}]`);
+  }
+};
+
+assertArraysEqual([1, 2, 3], [1, 2, 3]); // => should PASS
+assertArraysEqual([1, 2, 3], [3, 2, 1]); // =>
+assertArraysEqual(["1", "2", "3"], ["1", "2", "3"]);
