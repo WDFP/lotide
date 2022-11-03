109ba9f3924168d14193526c9b4057dc36031972
Victor Ha <victorha88@gmail.com>
2022-11-03 13:47:34 -0400

    Refactor assertArraysEqual.js-M1W4

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,21 +1,8 @@
 // FUNCTION IMPLEMENTATION
 
-const eqArrays = function (array1, array2) {
-  if (array1.length !== array2.length) {
-    return false;
-  }
-  // need loop to loop through each array to see if the elements are the same and true
-  for (i = 0; i < array1.length; i++) {
-    // see if elements are the same
-    // compare all elements in array1 and array2 with the correct indexes.
-    if (array1[i] !== array2[i]) {
-      return false;
-    }
-  }
-  return true;
-};
+const eqArrays = require("./eqArray");
 
-const assertArraysEqual = function (array1, array2) {
+const assertArraysEqual = function(array1, array2) {
   if (eqArrays(array1, array2)) {
     console.log(`👍👌🔔 Assertion Passed: [${array1}] === [${array2}]`);
   } else {
@@ -23,8 +10,4 @@ const assertArraysEqual = function (array1, array2) {
   }
 };
 
-
-assertArraysEqual([1, 2, 3], [1, 2, 3]); // => should PASS
-assertArraysEqual([1, 2, 3], [3, 2, 1]); // =>
-assertArraysEqual(["1", "2", "3"], ["1", "2", "3"]);
-assertArraysEqual(["1", "2", "3"], ["1", "2", 3]);
+module.exports = assertArraysEqual;
