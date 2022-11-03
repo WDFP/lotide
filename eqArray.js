a9f168d33de598550afc74f2dbcda41cc93c79ae
Victor Ha <victorha88@gmail.com>
2022-11-03 13:37:37 -0400

    Refactor eqArray.js-M1W4

--- eqArray.js
+++ eqArray.js
@@ -1,21 +1,8 @@
 // FUNCTION IMPLEMENTATION
 
-const assertEqual = function (actual, expected) {
-  if (actual === expected) {
-    console.log(`👍👌🔔 Assertion Passed: ${actual} === ${expected}`);
-  } else if (actual !== expected) {
-    console.log(`👎🙀🔕 Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
-// TEST CODE
+const assertEqual = require("./assertEqual");
 
-// assertEqual("Bootcamp", "Bootcamp");
-// assertEqual(5, 4);
-
-const eqArrays = function (arrays1, arrays2) {
-  // if (arrays1 === arrays2) {
-  //   return true;
-  //   }  else {
+const eqArrays = function(arrays1, arrays2) {
   if (arrays1.length !== arrays2.length) {
     return false;
   }
@@ -30,10 +17,4 @@ const eqArrays = function (arrays1, arrays2) {
   return true;
 };
 
-console.log(eqArrays([1, 2, 3], [1, 2, 3])); // => true
-console.log(eqArrays([1, 2, 3], [3, 2, 1])); // => false
-
-console.log(eqArrays(["1", "2", "3"], ["1", "2", "3"])); // => true
-console.log(eqArrays(["1", "2", "3"], ["1", "2", 3])); // => false
-
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS
+module.exports = eqArrays;
